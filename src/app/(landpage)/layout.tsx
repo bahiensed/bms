85fa1757fab5bb670acf4766a5ecca32d22f2d60
Douglas Bahiense <douglas@rohling.com.br>
2026-03-24 12:11:43 -0300

    insert an  inactive search input on landing page's header

--- src/app/(landpage)/layout.tsx
+++ src/app/(landpage)/layout.tsx
@@ -1,4 +1,5 @@
 import { ModeToggle } from '@/components/theme/mode-toggle'
+import { SearchInput } from '@/components/search/search-input'
 
 export default function LandingPageLayout({
   children,
@@ -6,9 +7,13 @@ export default function LandingPageLayout({
   children: React.ReactNode
 }>) {
   return (
-        <div className="flex flex-col w-full min-h-screen">
+        <div className="flex flex-col min-h-screen">
           <div className="flex border-b items-center justify-between p-2">
-            <ModeToggle />
+            Basic Boilerplate
+            <div className="flex gap-2 items-center">
+              <SearchInput />
+              <ModeToggle />
+            </div>
           </div>
 
           <main className="flex-1 overflow-y-auto p-4">
