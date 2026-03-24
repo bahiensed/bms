acd4654e3e7f8c4758c101dce91fa9c5c3f851f1
Douglas Bahiense <douglas@rohling.com.br>
2026-03-24 12:13:57 -0300

    insert an  inactive search input on protected pages' header

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/sidebar/app-sidebar"
 import { ModeToggle } from "@/components/theme/mode-toggle";
+import { SearchInput } from "@/components/search/search-input";
 
 export default function Layout({
   children,
@@ -20,7 +21,10 @@ export default function Layout({
         <div className="flex flex-col w-full min-h-screen">
           <div className="flex border-b items-center justify-between p-2">
             <SidebarTrigger />
-            <ModeToggle />
+            <div className="flex gap-2 items-center">
+              <SearchInput />
+              <ModeToggle />
+            </div>
           </div>
 
           <main className="flex-1 overflow-y-auto p-4">
